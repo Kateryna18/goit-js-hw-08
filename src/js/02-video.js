586592b3094d58d291f4440e35020c08d65d7368
Kateryna18 <mikolaichukkate@icloud.com>
2023-03-03 16:31:53 +0200

    in procces task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+console.log(player)
+
+player.on('timeupdate', function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+});
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+const number = +currentTime;
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime).then(function() {
+    player.play();
+  });
+}
