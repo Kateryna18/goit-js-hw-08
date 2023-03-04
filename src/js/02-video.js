09baa241063a5a542b7cc1afdf9c3c5133324ac3
Kateryna18 <mikolaichukkate@icloud.com>
2023-03-04 13:04:25 +0200

    done task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,24 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-console.log(player)
+const VIDEO_PROGRESS_KEY = "videoplayer-current-time";
 
-player.on('timeupdate', function(data) {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
-});
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+function onProgress({seconds}) {
+  localStorage.setItem(VIDEO_PROGRESS_KEY, seconds);
+
+}
+
+player.on('timeupdate', throttle(onProgress, 1000));
+
+const storedTime = localStorage.getItem(VIDEO_PROGRESS_KEY);
+
+if(storedTime) {
+  player.setCurrentTime(storedTime) 
+    
+}
+
 
